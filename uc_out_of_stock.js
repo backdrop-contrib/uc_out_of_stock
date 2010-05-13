058ec3d91501a5e2fb860e2444b66ead72a970df
Ariel Barreiro <abarrei@gmail.com>
2010-05-13 20:21:53 +0000

    Latest merge from DRUPAL-6--1 branch into HEAD for branching DRUPAL-6--2

--- uc_out_of_stock.js
+++ uc_out_of_stock.js
@@ -1,19 +1,19 @@
 // $Id$
 
-$(document).ready(function(){
+Drupal.behaviors.ucOutOfStock =  function() {
   // Your code here
-  //$("form[@id*=uc-product-add-to-cart-form]").css("border","2px solid");
-  formid = 'uc-product-add-to-cart-form';
   attrid = 'edit-attributes';
 
   function checkStock(form) {
     var product = new Object();
     var attributes = new Object();
-    product.nid = form.attr('id').substring(formid.length+1);
+    var formid_data = new Array();
+
+    product.nid = form.attr('id').match(/(?:uc-product-add-to-cart-form-|catalog-buy-it-now-form-)([0-9]+)/)[1];
     attributes.found = new Object();
     attributes.value = new Object();
 
-    $(":input[@name*=attributes]:not(:text)", form).each(function(index){
+    $(":input[name*=attributes]:not(:text)", form).each(function(index){
       id = $(this).attr('name').substring(11,$(this).attr('name').length-1);
       if ($(this).is(':radio')) {
         attributes.found['attr'+id] = 1;
@@ -32,10 +32,12 @@ $(document).ready(function(){
       }
     });
 
-    // Put back the normal HTML of the add to cart form before deciding
-    // if a request to the server is needed.
-    $(".uc_out_of_stock_html", form).html('');
-    $("input:submit", form).show();
+    // find qty
+    product['qty'] = 1;
+    qty = $(":input[name=qty]", form).val()
+    if (qty) {
+      product['qty'] = qty;
+    }
 
     // finding if attributes are found with no value
     attributes.found.length = attributes.value.length = 0;
@@ -50,11 +52,14 @@ $(document).ready(function(){
       }
     }
     if (attributes.found.length != attributes.value.length) {
+      // Put back the normal HTML of the add to cart form
+      $(".uc_out_of_stock_html", form).html('');
+      $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).show();
       return;
     }
 
     $(".uc_out_of_stock_throbbing", form).addClass('uc_oos_throbbing');
-    $.post(Drupal.settings.base_path+'uc_out_of_stock/query', product, function (data, textStatus) {
+    $.post(Drupal.settings.basePath+'uc_out_of_stock/query', product, function (data, textStatus) {
       // textStatus can be one of:
       //   "timeout"
       //   "error"
@@ -65,26 +70,47 @@ $(document).ready(function(){
       stock = data[0];
       if (stock == parseInt(stock) && stock <= 0 && data.length == 2) {
         html = data[1];
-        $("input:submit", form).hide();
+        $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).hide();
         $(".uc_out_of_stock_html", form).html(html);
+      } else {
+        // Put back the normal HTML of the add to cart form
+        $(".uc_out_of_stock_html", form).html('');
+        $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).show();
       }
 
       $(".uc_out_of_stock_throbbing", form).removeClass('uc_oos_throbbing');
     });
   }
 
-  $("form[@id*=uc-product-add-to-cart-form]").each(function(index) {
+  $("form[id*=uc-product-add-to-cart-form]").each(function(index) {
     var eachForm;
-    $("input:submit", $(this)).before('<div class="uc_out_of_stock_throbbing">&nbsp;&nbsp;&nbsp;&nbsp;</div> ');
-    $("input:submit", $(this)).after('<div class="uc_out_of_stock_html"></div');
+    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).before('<div class="uc_out_of_stock_throbbing">&nbsp;&nbsp;&nbsp;&nbsp;</div> ');
+    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).after('<div class="uc_out_of_stock_html"></div');
 
     eachForm = $(this);
     checkStock(eachForm);
 
-    $(":input[@name*=attributes]:not(:text)", $(this)).change(function(){
+    $(":input[name*=attributes]:not(:text)", $(this)).change(function(){
       checkStock(eachForm);
     });
-
+    /* TODO: Feature request - support qty field, would make sense if cart
+     * contents are checked in the server as well as just stock
+     */
+    /*
+    $(":input[name=qty]", $(this)).keyup(function(){
+      checkStock(eachForm);
+    });
+    */
   });
 
-});
+  $("form[id*=uc-catalog-buy-it-now-form]").each(function(index) {
+    var eachForm;
+    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).before('<div class="uc_out_of_stock_throbbing">&nbsp;&nbsp;&nbsp;&nbsp;</div> ');
+    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).after('<div class="uc_out_of_stock_html"></div');
+
+    eachForm = $(this);
+    checkStock(eachForm);
+
+    // This form has no possible attributes
+  });
+};
