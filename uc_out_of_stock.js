9f803ead62f395c6084a711e88ab024f0ae7a97d
Ariel Barreiro <abarrei@gmail.com>
2011-09-24 16:44:57 -0300

    - #514170 by jrust, hanoii, djflux | Vuds: Added Make the module less process consuming.

--- uc_out_of_stock.js
+++ uc_out_of_stock.js
@@ -2,93 +2,108 @@ Drupal.behaviors.ucOutOfStock =  function() {
   // Your code here
   attrid = 'edit-attributes';
 
-  function checkStock(form) {
-    var product = new Object();
-    var attributes = new Object();
+  function checkStock(forms) {
+    var form_ids = new Array();
+    var node_ids = new Array();
+    var attr_ids = new Array();
+    $.each(forms, function(index, form) {
+      var product = new Object();
+      var attributes = new Object();
 
-    product.nid = form.attr('id').match(/(?:uc-product-add-to-cart-form-|catalog-buy-it-now-form-)([0-9]+)/)[1];
-    attributes.found = new Object();
-    attributes.value = new Object();
+      var nid = form.attr('id').match(/(?:uc-product-add-to-cart-form-|catalog-buy-it-now-form-)([0-9]+)/)[1];
 
-    $(":input[name*=attributes]:not(:text)", form).each(function(index){
-      id = $(this).attr('name').substring(11,$(this).attr('name').length-1);
-      if ($(this).is(':radio')) {
-        attributes.found['attr'+id] = 1;
-        if ($(this).is(':checked')) {
-          if ($(this).val()) {
-            attributes.value['attr'+id] = 1;
-            product['attr'+id] = $(this).val();
+      attributes.found = new Object();
+      attributes.value = new Object();
+
+      $(":input[@name*=attributes]:not(:text)", form).each(function(index){
+        id = $(this).attr('name').substring(11,$(this).attr('name').length-1);
+        if ($(this).is(':radio')) {
+          attributes.found['attr'+id] = 1;
+          if ($(this).is(':checked')) {
+            if ($(this).val()) {
+              attributes.value['attr'+id] = 1;
+              attr_ids.push(nid + ':' + id + ':' + $(this).val());
+            }
           }
         }
-      } else {
-      attributes.found['attr'+id] = 1;
-        if ($(this).val()) {
-          attributes.value['attr'+id] = 1;
-          product['attr'+id] = $(this).val();
+        else {
+          attributes.found['attr'+id] = 1;
+          if ($(this).val()) {
+            attributes.value['attr'+id] = 1;
+            attr_ids.push(nid + ':' + id + ':' + $(this).val());
+          }
+        }
+      });
+
+      // find qty
+      product['qty'] = 1;
+      qty = $(":input[name=qty]", form).val()
+      if (qty) {
+        product['qty'] = qty;
+      }
+
+      // finding if attributes are found with no value
+      attributes.found.length = attributes.value.length = 0;
+      for (var i in attributes.found) {
+        if (i!='length') {
+          attributes.found.length++;
         }
       }
-    });
 
-    // find qty
-    product['qty'] = 1;
-    qty = $(":input[name=qty]", form).val()
-    if (qty) {
-      product['qty'] = qty;
-    }
-
-    // finding if attributes are found with no value
-    attributes.found.length = attributes.value.length = 0;
-    for (var i in attributes.found) {
-      if (i!='length') {
-        attributes.found.length++;
+      for (var i in attributes.value) {
+          if (i!='length') {
+          attributes.value.length++;
+        }
       }
-    }
-    for (var i in attributes.value) {
-      if (i!='length') {
-        attributes.value.length++;
-      }
-    }
-    if (attributes.found.length != attributes.value.length) {
-      // Put back the normal HTML of the add to cart form
-      $(".uc_out_of_stock_html", form).html('');
-      $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).show();
-      return;
-    }
-
-    $(".uc_out_of_stock_throbbing", form).addClass('uc_oos_throbbing');
-    $.post(Drupal.settings.basePath+'uc_out_of_stock/query', product, function (data, textStatus) {
-      // textStatus can be one of:
-      //   "timeout"
-      //   "error"
-      //   "notmodified"
-      //   "success"
-      //   "parsererror"
-      data = data.split('|');
-      stock = data[0];
-      if (stock == parseInt(stock) && stock <= 0 && data.length == 2) {
-        html = data[1];
-        $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).hide();
-        $(".uc_out_of_stock_html", form).html(html);
-      } else {
+      if (attributes.found.length != attributes.value.length) {
         // Put back the normal HTML of the add to cart form
         $(".uc_out_of_stock_html", form).html('');
         $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).show();
+        return;
       }
 
-      $(".uc_out_of_stock_throbbing", form).removeClass('uc_oos_throbbing');
+      $(".uc_out_of_stock_throbbing", form).addClass('uc_oos_throbbing');
+      form_ids.push(form.attr('id'));
+      node_ids.push(nid);
+    });
+
+    if (form_ids.length == 0) {
+      return;
+    }
+
+    var post = { 'form_ids[]': form_ids, 'node_ids[]': node_ids, 'attr_ids[]': attr_ids }
+    $.ajax({
+      type: 'post',
+      url : Drupal.settings.basePath+'uc_out_of_stock/query',
+      data: post,
+      success : function (data, textStatus) {
+        $.each(data, function(form_id, stock_level) {
+          var form = $('#' + form_id);
+          if (stock_level != null && parseInt(stock_level) <= 0) {
+            $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).hide();
+            $(".uc_out_of_stock_html", form).html(Drupal.settings.uc_out_of_stock.msg);
+          }
+          else {
+            // Put back the normal HTML of the add to cart form
+            $(".uc_out_of_stock_html", form).html('');
+            $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", form).show();
+          }
+          $(".uc_out_of_stock_throbbing", form).removeClass('uc_oos_throbbing');
+        });
+      },
+      dataType: 'json'
     });
   }
 
-  $("form[id*=uc-product-add-to-cart-form]").each(function() {
-    var eachForm;
-    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).before('<div class="uc_out_of_stock_throbbing">&nbsp;&nbsp;&nbsp;&nbsp;</div> ');
-    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).after('<div class="uc_out_of_stock_html"></div');
-
-    eachForm = $(this);
-    checkStock(eachForm);
+  var forms = new Array();
+  $("form[id*=uc-product-add-to-cart-form], form[id*=uc-catalog-buy-it-now-form]").each(function() {
+    forms.push($(this));
+    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).before('<div class="uc_out_of_stock_throbbing">&nbsp;&nbsp;&nbsp;&nbsp;</div>');
+    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).after('<div class="uc_out_of_stock_html"></div>');
+    var form = $(this);
 
     $(":input[name*=attributes]:not(:text)", $(this)).change(function(){
-      checkStock(eachForm);
+      checkStock([form]);
     });
     /* TODO: Feature request - support qty field, would make sense if cart
      * contents are checked in the server as well as just stock
@@ -100,15 +115,6 @@ Drupal.behaviors.ucOutOfStock =  function() {
     */
   });
 
-  $("form[id*=uc-catalog-buy-it-now-form]").each(function(index) {
-    var eachForm;
-    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).before('<div class="uc_out_of_stock_throbbing">&nbsp;&nbsp;&nbsp;&nbsp;</div> ');
-    $("input:submit.node-add-to-cart,input:submit.list-add-to-cart", $(this)).after('<div class="uc_out_of_stock_html"></div');
-
-    eachForm = $(this);
-    checkStock(eachForm);
-
-    // This form has no possible attributes
-  });
+  checkStock(forms);
 };
 
